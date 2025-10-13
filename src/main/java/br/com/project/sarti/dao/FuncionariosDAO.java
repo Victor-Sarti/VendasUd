bd3ae4a74367b2d5d1b60958312b8a8b30d15e03
Victor-sarti <Sartivictor24@gmail.com>
2025-10-13 13:28:55 -0300

    create method excluir and alterar funcionarios

--- src/main/java/br/com/project/sarti/dao/FuncionariosDAO.java
+++ src/main/java/br/com/project/sarti/dao/FuncionariosDAO.java
@@ -66,6 +66,71 @@ public class FuncionariosDAO {
         
     }
     
+      //alterar funcionario 
+      public void alterarFuncionario(Funcionarios obj){
+         try {
+            String sql = "UPDATE tb_funcionarios SET nome=? ,rg=? ,cpf=? ,email=?, senha=?,cargo=?,nivel_acesso=? ,celular=? ,cep=? ,endereco=? ,numero=?,"
+                    + "complemento=?, bairro=?, cidade=?, estado=? WHERE id=?";
+                                                      
+        
+        // 2- conectar com o bd e organizar o comando sql
+           PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5,obj.getSenha());
+            stmt.setString(6,obj.getCargo());
+            stmt.setString(7,obj.getNivel_acesso());
+            stmt.setString(8, obj.getCelular());
+            stmt.setString(9, obj.getCep());
+            stmt.setString(10, obj.getEndereco());
+            stmt.setInt(11, obj.getNumero());
+            stmt.setString(12, obj.getComplemento());
+            stmt.setString(13, obj.getBairro());
+            stmt.setString(14, obj.getCidade());
+            stmt.setString(15, obj.getUf());
+            
+            stmt.setInt(16, obj.getId());
+            
+        // 3 - executar o comando sql
+        
+        stmt.execute();
+        stmt.close();
+        
+            JOptionPane.showMessageDialog(null,"Alterado com Sucesso!!");
+            
+        } catch (SQLException erro) {
+                        JOptionPane.showMessageDialog(null,"Erro" + erro);
+
+        }
+        
+    }
+      
+      
+
+    //excluir funcionario 
+      public void excluirFuncionario(Funcionarios obj){
+             // 1- criar comando sql
+        try {
+            String sql = " delete from tb_funcionarios where id = ?";
+        
+        // 2- conectar com o bd e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, obj.getId());    
+        // 3 - executar o comando sql
+        
+        stmt.execute();
+        stmt.close();
+        
+            JOptionPane.showMessageDialog(null,"Excluido com Sucesso!!");
+            
+        } catch (SQLException erro) {
+                        JOptionPane.showMessageDialog(null,"Erro" + erro);
+
+        }
+    }
+      
       //metodo listar Funcionarios    
     public List<Funcionarios> listarFuncionarios(){
         
