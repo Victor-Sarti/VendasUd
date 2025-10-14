e30de9fe61dfa74bef8991ab335bf970c505146a
Victor-sarti <Sartivictor24@gmail.com>
2025-10-14 16:00:25 -0300

    create method listar, consultar, e buscar por cnpj  fornecedores

--- src/main/java/br/com/project/sarti/dao/FornecedoresDAO.java
+++ src/main/java/br/com/project/sarti/dao/FornecedoresDAO.java
@@ -4,7 +4,10 @@ import br.com.project.jdbc.ConnectionFactory;
 import br.com.project.sarti.model.Fornecedores;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class FornecedoresDAO {
@@ -86,6 +89,90 @@ public class FornecedoresDAO {
         
     }
     
+     public List<Fornecedores> listarFornecedores(){
+        
+        try {
+            //1- criar a lista
+            List<Fornecedores> lista = new ArrayList<>();
+            
+            //2- criar o sql, organizar e executar
+            String sql = "select * from tb_fornecedores";
+            
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                Fornecedores obj = new Fornecedores();
+                
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setCnpj(rs.getString("cnpj"));
+                obj.setEmail(rs.getString("email"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("Complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+                
+                lista.add(obj);
+            }
+            
+            return lista;
+            
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+            return null;
+        }
+        
+    }
+     //listar fornecedores por nome
+       public List<Fornecedores> listarFornecedoresPorNome(String nome){
+        
+        try {
+            //1- criar a lista
+            List<Fornecedores> lista = new ArrayList<>();
+            
+            //2- criar o sql, organizar e executar
+            String sql = "select * from tb_fornecedores where nome like ?";
+            
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, nome);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                Fornecedores obj = new Fornecedores();
+                
+               obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setCnpj(rs.getString("cnpj"));
+                obj.setEmail(rs.getString("email"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("Complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+                
+                lista.add(obj);
+            }
+            
+            return lista;
+            
+        } catch (SQLException erro) {
+            
+            JOptionPane.showMessageDialog(null, "Erro:" + erro);
+            return null;
+        }
+        
+    }
+
+    
     //excluir fornecedores 
     
     public void excluirCliente(Fornecedores obj){
@@ -108,4 +195,44 @@ public class FornecedoresDAO {
 
         }
     }
+    
+    //consulta o fornecedore por cnpj
+     public Fornecedores consultaFornecedoresPorCnpj(String cnpj){
+        try {
+           
+            //1- criar o sql, organizar e executar
+            String sql = "select * from tb_fornecedores where cnpj = ?";
+            
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, cnpj);
+            ResultSet rs = stmt.executeQuery();
+            Fornecedores obj = new Fornecedores();
+
+            if(rs.next()){
+                
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setCnpj(rs.getString("cnpj"));
+                obj.setEmail(rs.getString("email"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("Complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+                
+            }
+            
+            return obj;
+            
+        } catch (Exception e) {
+            
+            JOptionPane.showMessageDialog(null, "Fornecedor não encontrado");
+            return null;
+        }
+        
+    }
+        
 }
