53acca21a73464d6f01d6c6fd2d597b02f530053
Victor-sarti <Sartivictor24@gmail.com>
2025-10-14 15:51:34 -0300

    create method excluir and alterar fornecedores

--- src/main/java/br/com/project/sarti/dao/FornecedoresDAO.java
+++ src/main/java/br/com/project/sarti/dao/FornecedoresDAO.java
@@ -49,4 +49,63 @@ public class FornecedoresDAO {
         }
 
     }
+    //metodo alterar Fornecedores    
+    public void alterarFornecedores(Fornecedores obj){
+         try {
+            String sql = "UPDATE tb_fornecedores SET nome=?,cnpj=? ,email=? ,celular=? ,cep=? ,endereco=? ,numero=?,"
+                    + "complemento=?, bairro=?, cidade=?, estado=? WHERE id=?";
+                                                      
+        
+       // 2- conectar com o bd e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getCnpj());
+            stmt.setString(3, obj.getEmail());
+            stmt.setString(4, obj.getCelular());
+            stmt.setString(5, obj.getCep());
+            stmt.setString(6, obj.getEndereco());
+            stmt.setInt(7, obj.getNumero());
+            stmt.setString(8, obj.getComplemento());
+            stmt.setString(9, obj.getBairro());
+            stmt.setString(10, obj.getCidade());
+            stmt.setString(11, obj.getUf());
+            
+            stmt.setInt(12, obj.getId());
+            
+        // 3 - executar o comando sql
+        
+        stmt.execute();
+        stmt.close();
+        
+            JOptionPane.showMessageDialog(null,"Alterado com Sucesso!!");
+            
+        } catch (SQLException erro) {
+                        JOptionPane.showMessageDialog(null,"Erro" + erro);
+
+        }
+        
+    }
+    
+    //excluir fornecedores 
+    
+    public void excluirCliente(Fornecedores obj){
+             // 1- criar comando sql
+        try {
+            String sql = " delete from tb_fornecedores where id = ?";
+        
+        // 2- conectar com o bd e organizar o comando sql
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, obj.getId());    
+        // 3 - executar o comando sql
+        
+        stmt.execute();
+        stmt.close();
+        
+            JOptionPane.showMessageDialog(null,"Excluido com Sucesso!!");
+            
+        } catch (SQLException erro) {
+                        JOptionPane.showMessageDialog(null,"Erro" + erro);
+
+        }
+    }
 }
