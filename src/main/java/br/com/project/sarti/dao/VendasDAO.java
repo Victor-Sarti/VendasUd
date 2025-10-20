1c161815b7add9980461ff29f2afb81daaf55d24
Victor-sarti <Sartivictor24@gmail.com>
2025-10-20 13:31:26 -0300

    create VendasDAO

---
+++ src/main/java/br/com/project/sarti/dao/VendasDAO.java
@@ -0,0 +1,67 @@
+
+package br.com.project.sarti.dao;
+
+import br.com.project.jdbc.ConnectionFactory;
+import br.com.project.sarti.model.Vendas;
+import com.mysql.cj.xdevapi.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class VendasDAO {
+       private Connection con;
+    
+    public VendasDAO(){
+        this.con = new ConnectionFactory().getConnection();
+        
+    }
+    
+    public void cadastrarVenda(Vendas obj){
+           try {
+            String sql = "insert into tb_vendas (cliente_id,data_venda,total_venda,observacoes) values(?,?,?,?)";
+             PreparedStatement stmt = con.prepareStatement(sql);
+             stmt.setInt(1, obj.getCliente().getId());
+             stmt.setString(2, obj.getData_venda());
+             stmt.setDouble(3, obj.getTotal_venda());
+             stmt.setString(4, obj.getObs());
+             
+             stmt.execute();
+             stmt.close();
+             
+              JOptionPane.showMessageDialog(null,"Venda Registrada com Sucesso!!");
+                
+            
+        } catch (Exception erro) {
+             JOptionPane.showMessageDialog(null,"Erro" + erro);
+            
+        }
+        }
+    
+    //retorna a ultima venda 
+    
+        public int retornaUltimaVenda(){
+            try {
+                int idvenda = 0;
+                
+                String sql = "select max(id) id from tb_vendas"; //comando do sql que faz retornar o maior id feito por ultimo
+                        
+                PreparedStatement ps = con.prepareStatement(sql);
+                
+                ResultSet rs = ps.executeQuery();
+                
+                if(rs.next()){
+                Vendas p = new Vendas();
+                p.setId(rs.getInt("id"));
+                idvenda = p.getId();
+                }
+                return idvenda;
+
+                        
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+    }
+}
+
--- src/main/java/br/com/project/sarti/model/Vendas.java
+++ src/main/java/br/com/project/sarti/model/Vendas.java
@@ -4,30 +4,52 @@
  */
 package br.com.project.sarti.model;
 
-public class Vendas {
+public class Vendas{
     //atributos
     private int id;
-    private Clientes Cliente;
+    private Clientes cliente;
     private String data_venda;
+    private double total_venda;
     private String obs;
-    
-    
-    //getters and setters
+
+    public int getId() {
+        return id;
+    }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public void setCliente(Clientes Cliente) {
-        this.Cliente = Cliente;
+    public Clientes getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Clientes cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getData_venda() {
+        return data_venda;
     }
 
     public void setData_venda(String data_venda) {
         this.data_venda = data_venda;
     }
 
+    public double getTotal_venda() {
+        return total_venda;
+    }
+
+    public void setTotal_venda(double total_venda) {
+        this.total_venda = total_venda;
+    }
+
+    public String getObs() {
+        return obs;
+    }
+
     public void setObs(String obs) {
         this.obs = obs;
     }
-    
+
 }
